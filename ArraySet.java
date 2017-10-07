029135412ac8373aa40e2d7e053d3bc9fe295ace
BigPapa76 <31287692+BigPapa76@users.noreply.github.com>
2017-10-07 15:10:02 -0500

    Add files via upload

--- ArraySet.java
+++ ArraySet.java
@@ -240,7 +240,56 @@ public class ArraySet<T extends Comparable<? super T>> implements Set<T> {
       // HINT: JUST USE THE SAME CODE/STRATEGY AS THE ARRAYBAG CLASS
       // FROM LECTURE. THE ONLY DIFFERENCE IS THAT YOU'LL NEED THE
       // ARRAYITERATOR CLASS TO BE NESTED, NOT TOP-LEVEL.
-   
+      
+      class ArrayIterator<T> implements Iterator<T> {
+      
+      // the array of elements to iterate over
+         private T[] elements;
+      // the number of elements in the array, beginning at index zero
+         private int count;
+      // the index of the next element in the iteration sequence
+         private int current;
+      
+      /**
+      * Construct a properly initialized iterator.
+      *
+      * @param  elem the array to be iterated over
+      * @param  size the number of elements in the array
+      */
+         public ArrayIterator(T[] elem, int size) {
+            elements = elem;
+            count = size;
+            current = 0;
+         }
+      
+      /**
+      * Returns true if there is at least one more element remaining
+      * in the iteration sequence.
+      *
+      * @return true if there is a next element to iterate over
+      */
+         public boolean hasNext() {
+            return current < count;
+         }
+      
+      /**
+      * Returns the next element in the iteration sequence.
+      * @return the next element in the iteration sequence
+      */
+         public T next() {
+            if (!hasNext()) {
+               throw new NoSuchElementException();
+            }
+            return elements[current++];
+         }
+      
+      /**
+      * Unsupported operation.
+      */
+         public void remove() {
+            throw new UnsupportedOperationException();
+         }
+      }
       return null;
    }
 
